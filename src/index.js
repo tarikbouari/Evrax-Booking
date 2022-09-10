e04cb5396039e5aff87d005b330b4cf2bf82b73c
Rikstone <tarikbouari@gmail.com>
2022-09-10 23:43:08 +0000

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./css/main.css";
-import "./css/desktop.css";
-import { GetHotels } from "./modules/api";
+import './css/main.css';
+import './css/desktop.css';
+import GetHotels from './modules/api.js';
 
 GetHotels();
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,19 +1,19 @@
-const key = "sandb_MgfCGdF7vkFG17Tb7eD4qr7SraWlIYsdaLE3b3Wu";
-const urlGetHotel = "https://sandbox.impala.travel/v1/hotels";
+const key = 'sandb_MgfCGdF7vkFG17Tb7eD4qr7SraWlIYsdaLE3b3Wu';
+const urlGetHotel = 'https://sandbox.impala.travel/v1/hotels';
 
 // Fetch data from API
 
 const GetHotels = async () => {
   const res = await fetch(urlGetHotel, {
-    mode: "no-cors",
+    mode: 'no-cors',
     headers: {
-      "Content-Type": "application/json",
-      "x-api-key": key,
-      "Accept-Encoding": "*/*",
+      'Content-Type': 'application/json',
+      'x-api-key': key,
+      'Accept-Encoding': '*/*',
     },
-  })
+  });
   const data = await res.json();
-  console.log(data);
+  return data;
 };
 
-export { GetHotels };
+export default GetHotels;
