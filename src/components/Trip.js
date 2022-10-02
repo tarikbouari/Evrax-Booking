6d88c789a42ea0ae66ac184aacaedae1e45c54a0
Rikstone <tarikbouari@gmail.com>
2022-10-02 23:26:12 +0000

    Create trip commponent

---
+++ src/components/Trip.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './trip.css'
+
+const Trip = () => {
+    
+    return (
+
+        <div className='Box-tripCard'> 
+            <div className="trip-card">
+          <div className="left-card">
+            <h3> Wild Trips</h3>
+            <button>Seee all</button>
+          </div>
+          <div className="right-card">
+
+          </div>
+      </div>
+      <div className="card">
+
+      </div>
+        </div>
+    )
+       
+}
+
+export default Trip;
