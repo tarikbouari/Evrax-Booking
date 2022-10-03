be44985a7ab5eee026b5c7e9566de57e2cfb4b1f
Rikstone <tarikbouari@gmail.com>
2022-10-03 01:04:14 +0000

    update file

--- src/components/api.js
+++ src/components/api.js
@@ -1,24 +1,38 @@
 // import React from 'react';
 
 const Api = () => {
-  const key = 'sandb_MgfCGdF7vkFG17Tb7eD4qr7SraWlIYsdaLE3b3Wu';
-  const urlGetHotel = 'https://sandbox.impala.travel/v1/hotels';
+  // const key = "sandb_rwTLUz7xYeyQS1Es6Nara0m6qLmbN4NU8KgVxYUC";
+  // const urlGetHotel =
+  //   "https://sandbox.impala.travel/v1/hotels";
 
   // Fetch data from API
 
-  const GetHotels = async () => {
-    const res = await fetch(urlGetHotel, {
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': key,
-        'Accept-Encoding': '*/*',
-      },
-    });
-    const data = await res.json();
-    return data;
+  // const GetHotels = async () => {
+  //   const res = await fetch(urlGetHotel, {
+  //     mode: "no-cors",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "x-api-key": key,
+  //       // "Accept-Encoding": "*/*",
+  //     },
+  //   });
+  //   const dataHotels = await res.json();
+  //   console.log(dataHotels);
+  //   return dataHotels;
+  // };
+  // GetHotels();
+  const options = {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': 'sandb_0gXBJfskPhf8BFSW6xJSqj9lneseXwKiJNhxefnq',
+    },
   };
-  GetHotels();
+  fetch('https://sandbox.impala.travel/v1/hotels', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 };
 
 export default Api;
