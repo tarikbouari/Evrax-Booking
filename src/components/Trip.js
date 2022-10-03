1b5cc3fae717225e83504c44da235170afd696cd
Rikstone <tarikbouari@gmail.com>
2022-10-03 01:03:01 +0000

    update trip component

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import './trip.css'
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from 'react-icons/bs';
+import './trip.css';
+import squad from '../images/squad.jpg';
 
-const Trip = () => {
-    
-    return (
-
-        <div className='Box-tripCard'> 
-            <div className="trip-card">
-          <div className="left-card">
-            <h3> Wild Trips</h3>
-            <button>Seee all</button>
-          </div>
-          <div className="right-card">
-
-          </div>
+const Trip = () => (
+  <div className="py-5">
+    <div className="trip-card d-flex justify-content-between py-3">
+      <div className="left-card d-flex">
+        <h3> Wild Trips</h3>
+        <button type="button">Seee all</button>
       </div>
-      <div className="card">
-
+      <div className="right-card d-flex">
+        <BsFillArrowLeftCircleFill className="arrow" />
+        <BsFillArrowRightCircleFill className="arrow" />
       </div>
+    </div>
+
+    <div className="box ">
+      <div className="Box-tripCard ">
+        <div className="card">
+          <div>
+            <img src={squad} alt="bike-rides" className="img-card" />
+          </div>
+          <div className="info-card">
+            <div className="d-flex">
+              <h4>Quad Bike RIDES</h4>
+              <span> Person 2</span>
+            </div>
+          </div>
         </div>
-    )
-       
-}
+      </div>
+
+      <div className="Box-tripCard ">
+        <div className="card">
+          <div>
+            <img src={squad} alt="bike-rides" className="img-card" />
+          </div>
+
+          <div className="info-card">
+            <div className="d-flex">
+              <h4>Quad Bike RIDES</h4>
+              <span> Person 2</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Trip;
